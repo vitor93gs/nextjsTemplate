87995c3c380572d603472bbe31c5ca47fecf2d0d
Vitor <vitor93gs@gmail.com>
2022-07-13 11:14:51 -0300

    criado template nextjs com tailwind

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Template</title>
+        <meta name="description" content="Template web app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div> </div>
+    </div>
+  );
+}
